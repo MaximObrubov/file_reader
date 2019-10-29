c6d88f6d631ee5ff3a82cb71beb7c051dd0330cb
Maxim Obrubov <m.obrubov@yandex.ru>
2019-10-29 22:09:54 +0400

    working non algorythmic solution

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -2,14 +2,43 @@ module NMax
   
   class Generator
     def self.test_string(length: 1000)
-      o = [('a'..'z'), (0..1000)].map(&:to_a).flatten
+      o = [('a'..'z'), (0..9)].map(&:to_a).flatten
       (0...length).map { o[rand(o.length)] }.join
     end
   end
   
+  
   class Reader
-    def self.read_char
-      puts "read char"
+    
+    MAX_LENGTH = 1000
+    
+    def self.get
+      unless (ARGV.count > 0 && numeric?(ARGV[0]))
+        raise "N parameter not passed"
+      end
+      n = ARGV[0].to_i
+      answer = []
+      current = ''
+      
+      $stdin.each_char do |char|
+        if numeric?(char)
+          current << char
+        else
+          answer << current.to_i unless current.empty?
+          current = ''
+        end
+      end
+      
+      answer.sort!.reverse!
+      
+      puts "=" * 50
+      p answer[0..n]
+      puts "=" * 50
     end
+    
+    def self.numeric?(str)
+      str.match?(/^\d+$/)
+    end
+    
   end
 end
