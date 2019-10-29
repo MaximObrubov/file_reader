910cd6bd0b7efe342da0d0ecf38a5b0da961c365
Maxim Obrubov <m.obrubov@yandex.ru>
2019-10-29 21:08:12 +0400

    added test string generator

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,4 +1,12 @@
 module NMax
+  
+  class Generator
+    def self.test_string(length: 1000)
+      o = [('a'..'z'), (0..1000)].map(&:to_a).flatten
+      (0...length).map { o[rand(o.length)] }.join
+    end
+  end
+  
   class Reader
     def self.read_char
       puts "read char"
---
+++ spec/test_nmax.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'nmax'
+
+class NMaxTest < Minitest::Test
+  
+  def test_getnums
+    assert_equal 1, 1 
+  end
+
+end
